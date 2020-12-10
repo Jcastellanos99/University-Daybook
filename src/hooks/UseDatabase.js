4d41c808450a4eb3174e7b7fe1cf450a27f8b742
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-12-09 21:58:07 -0600

    Creación del Hooks

---
+++ src/hooks/UseDatabase.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { database } from "../components/db";
+
+const useDatabase = () => {
+  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
+
+  const loadDatabase = async () => {
+    try {
+      // await database.dropDatabaseTableAsync();
+      // await database.setupDatabaseTableAsync();
+      // await database.setupNotesAsync();
+
+      // Finaliza la carga de la DB
+      setIsLoadingComplete(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadDatabase();
+  }, []);
+
+  return isLoadingComplete;
+};
+
+export default useDatabase;
