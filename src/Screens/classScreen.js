002564562537574c474e718c76721bf686c69f38
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-12-09 21:57:09 -0600

    Diseño de la pantalla de asignaturas

--- src/Screens/classScreen.js
+++ src/Screens/classScreen.js
@@ -1,15 +1,39 @@
-import React, { useEffect, useState } from "react";
+import { Container, Content, Fab, List, ListItem, Icon } from "native-base";
+import React, { useContext, useEffect, useState } from "react";
 import {StyleSheet, View, Text} from "react-native";
-import {Button, Container} from "native-base";
+
+
+//Importar el contexto de las notas
+import {ClassContext} from "../context/classContext";
 
 
 const ClassScreen=({navigation}) => {
+    const {class_} = useContext(ClassContext);
+
     return(
         <Container>
-        <Button rounded onPress={() => {navigation.navigate("Agregar Asignatura")}}>
-            <Text>Agregar Asignatura</Text>
-        </Button>           
-    </Container>
+            <Content>
+                <List>
+                    {class_
+                    ? class_.map((_class_) =>(
+                        <ListItem key={class_.idClass.toString()}>
+                            <Text>{class_._class_}</Text>
+                        </ListItem>
+                    ))
+                : null}
+                </List>
+                <Fab 
+                    active={true}
+                    position="bottomRight"
+                    style={{backgroundColor: "#ff0023"}}
+                    direction="up"
+                    onPress={() =>{
+                        navigation.navigate("classCreate")
+                    }}>
+                        <Icon name="plus"/>
+                    </Fab>
+            </Content>
+        </Container>
     );
 }
 
